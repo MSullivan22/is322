3c9a48266ab8d8f12436bccaee1c2a7458cb49e0
Michael Sullivan <mfs8@njit.edu>
2014-02-06 15:34:44 -0500

    Added auxiliary content loader

--- js/main.js
+++ js/main.js
@@ -9,12 +9,70 @@ function checkMobile() {
   mobile = (sw > breakpoint) ? false : true;
 	
   if (!mobile) { //If Not Mobile
-    $('[role="tabpanel"],#nav,#search').show(); //Show full
-	        navigation and search
-	          } else { //Hide 
+    $('[role="tabpanel"],#nav,#search').show(); //Show full navigation and search
+    loadAux(); //Load auxiliary content
+		 } else { //Hide 
 	              if(!$('#nav-anchors a').hasClass('active')) {
-	                    $('#nav,#search').hide(); //Hide full navigation
-	                    and search
+	                    $('#nav,#search').hide(); //Hide full navigation and search
 			}          
 		}
 	}
+
+function buildGallery() {
+  container.html('<div id="img-list"><ul /></div>');
+    imgList = $('#img-list');
+      nav.find('a:first').addClass('active');
+        
+	  //For Each Navigation Link
+	    nav.find('a').each(function() {
+	        var $this = $(this);
+	            var href = $this.attr('href');
+	                  
+	                      //Prepare list item with image source in data
+	                      attribute
+	                          arr += '<li data-imgsrc="'+href+'"></li>';
+	                            });
+	                              
+	                                //Append to #img-list
+	                                  imgList.find('ul').append(arr);
+	                                        
+	                                          //Nav Thumbnail Click
+	                                            nav.on('click', 'a',
+	                                            function(e) {
+	                                                var pos =
+	                                                $(this).parent().index();
+	                                                   e.preventDefault();
+	                                                        loadImg(pos);
+	                                                            if(swipeEnabled)
+	                                                            {
+	                                                                  mySwipe.slide(index,
+	                                                                  300);
+	                                                                      }
+	                                                                          updateNav(pos);
+	                                                                            });
+
+}
+
+//Set up Auxiliary content
+function loadAux() {
+  var $aux = $('.aux');
+    $aux.each(function(index) {
+        var $this = $(this);
+            var auxLink = $this.find('a');
+                var auxFragment = auxLink.attr('href');
+                    var auxContent = $this.find('[role=tabpanel]');
+                        if (auxContent.size()===0 &&
+                        $this.hasClass('loaded')===false) {
+                              loadContent(auxFragment,$this);
+                                  }
+                                    });
+                                    }
+
+                                    function loadContent(src,container) { //
+                                    Load Tab Content
+                                      container.addClass('loaded');
+                                        $('<div role="tabpanel" />').load(src
+                                        +' #content > div',function() {
+                                            $(this).appendTo(container);
+                                              });
+                                              }
