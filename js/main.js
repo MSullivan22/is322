381d0e942214a02f5924d25d837ac683ef7ad56d
Michael Sullivan <mfs8@njit.edu>
2014-02-09 01:51:00 -0500

    Added setImage

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+
+        
+(function(w){
+
 $(w).resize(function(){ //Update dimensions on resize
   sw = document.documentElement.clientWidth;
   sh = document.documentElement.clientHeight;
   checkMobile();
 });
-        
+
 //Check if Mobile
 function checkMobile() {
   mobile = (sw > breakpoint) ? false : true;
@@ -18,61 +22,123 @@ function checkMobile() {
 		}
 	}
 
-function buildGallery() {
-  container.html('<div id="img-list"><ul /></div>');
-    imgList = $('#img-list');
-      nav.find('a:first').addClass('active');
-        
-	  //For Each Navigation Link
-	    nav.find('a').each(function() {
-	        var $this = $(this);
-	            var href = $this.attr('href');
-	                  
-	                      //Prepare list item with image source in data
-	                      attribute
-	                          arr += '<li data-imgsrc="'+href+'"></li>';
-	                            });
-	                              
-	                                //Append to #img-list
-	                                  imgList.find('ul').append(arr);
-	                                        
-	                                          //Nav Thumbnail Click
-	                                            nav.on('click', 'a',
-	                                            function(e) {
-	                                                var pos =
-	                                                $(this).parent().index();
-	                                                   e.preventDefault();
-	                                                        loadImg(pos);
-	                                                            if(swipeEnabled)
-	                                                            {
-	                                                                  mySwipe.slide(index,
-	                                                                  300);
-	                                                                      }
-	                                                                          updateNav(pos);
-	                                                                            });
-
-}
+//Set up Image Carousel
+	function setImg() {
+		var container = $('#img-container'),
+			nav = $('#product-img').find('nav'),
+			imgList = Object,
+			current = 0,
+			swipeEnabled = false;
+		
+		function buildGallery() {
+			container.html('<div id="img-list"><ul /></div>');
+			imgList = $('#img-list');
+			nav.find('li:first').addClass('active');
+			
+			var arr = '';
+			
+			//For Each Navigation Link
+			nav.find('a').each(function() {
+				var $this = $(this),
+					href = $this.attr('href');
+					
+				//Prepare list item with image source in data attribute
+				arr += '<li data-imgsrc="'+href+'"></li>';
+			});
+			
+			//Append to #img-list
+			imgList.find('ul').append(arr);
+			
+			//Nav Thumbnail Click
+			nav.on('click','a',function(e) {
+				var pos = $(this).parent().index();
+				e.preventDefault();
+				loadImg(pos);
+				if(swipeEnabled) {
+					mySwipe.slide(pos,300);
+				}
+				updateNav(pos);
+			});
+			
+			Modernizr.load({
+			  test: Modernizr.touch && Modernizr.csstransitions,
+			  yep : 'js/swipe.js',
+			  complete : function() {
+					if(w.Swipe) {
+						swipeEnabled = true;
+						buildSwipe();
+					}
+			  }
+			});
+			loadImg(0); //Load initial image
+		}
+		
+		//Build Swipe Carousel
+		function buildSwipe() {
+			//Initialize Swipe.js
+			var imgList = document.getElementById('img-list');
+			w.mySwipe = new Swipe(imgList, {
+				callback: function(event, index, elem) {
+					updateNav(index);
+					loadImg(index+1);
+				}	
+			});
+			imgList.addEventListener('touchstart', function(event) {
+    			loadImg(w.mySwipe.getPos()+1);
+			}, false);
+		}
+		
+		//Dynamically Load Images
+		function loadImg(index) {
+			var lis = imgList.find('li'),
+				li = lis.eq(index),
+				imgSrc = li.attr('data-imgsrc');
+			if(!swipeEnabled) {
+				lis.hide();
+				li.show();
+			}
+			
+			if(li.html() === "") { //If li is empty
+				var img = new Image();
+				imgList.addClass('loading');
+				$(img).load(function () { //Load image
+					$(this).hide();
+					li.removeClass('loading');
+					$(this).fadeIn();
+				}).error(function () {
+					// notify the user that the image could not be loaded
+				}).attr('src', imgSrc);
+				$('<img />').attr('src',imgSrc).appendTo(li);
+			}
+		}
+		
+		//Update Image Thumbnail Navigation
+		function updateNav(pos) {
+			nav.find('li').removeClass('active');
+			nav.find('li:eq('+pos+')').addClass('active');
+		}
+		
+		buildGallery();
+	}
 
 //Set up Auxiliary content
 function loadAux() {
-  var $aux = $('.aux');
+	var $aux = $('.aux');
     $aux.each(function(index) {
-        var $this = $(this);
-            var auxLink = $this.find('a');
-                var auxFragment = auxLink.attr('href');
-                    var auxContent = $this.find('[role=tabpanel]');
-                        if (auxContent.size()===0 &&
-                        $this.hasClass('loaded')===false) {
-                              loadContent(auxFragment,$this);
-                                  }
-                                    });
-                                    }
+		var $this = $(this);
+		var auxLink = $this.find('a');
+		var auxFragment = auxLink.attr('href');
+		var auxContent = $this.find('[role=tabpanel]');
+		if (auxContent.size()===0 && $this.hasClass('loaded')===false) {
+			loadContent(auxFragment,$this);
+		}
+    });
+}
 
-                                    function loadContent(src,container) { //
-                                    Load Tab Content
-                                      container.addClass('loaded');
-                                        $('<div role="tabpanel" />').load(src
-                                        +' #content > div',function() {
-                                            $(this).appendTo(container);
-                                              });
-                                              }
+function loadContent(src,container) { // Load Tab Content
+    container.addClass('loaded');
+    $('<div role="tabpanel" />').load(src +' #content > div',function() {
+        $(this).appendTo(container);
+	});
+}
+}
