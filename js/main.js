cff1ae523cd40c1ca9127917db00dcca1381de12
Michael Sullivan <mfs8@njit.edu>
2014-02-09 01:54:00 -0500

    Added setNav

--- js/main.js
+++ js/main.js
@@ -2,27 +2,58 @@
         
 (function(w){
 
-$(w).resize(function(){ //Update dimensions on resize
-  sw = document.documentElement.clientWidth;
-  sh = document.documentElement.clientHeight;
-  checkMobile();
-});
+	var sw = document.body.clientWidth,
+			sh = document.body.clientHeight,
+			breakpoint = 650,
+			speed = 800,
+			mobile = true;
+			
+		$(document).ready(function() {
+			checkMobile();
+			setNav();
+			setImg();
+		});
 
-//Check if Mobile
-function checkMobile() {
-  mobile = (sw > breakpoint) ? false : true;
+	$(w).resize(function(){ //Update dimensions on resize
+	  sw = document.documentElement.clientWidth;
+	  sh = document.documentElement.clientHeight;
+	  checkMobile();
+	});
+
+	//Check if Mobile
+	function checkMobile() {
+	  mobile = (sw > breakpoint) ? false : true;
+		
+	  if (!mobile) { //If Not Mobile
+		$('[role="tabpanel"],#nav,#search').show(); //Show full navigation and search
+		loadAux(); //Load auxiliary content
+			 } else { //Hide 
+					  if(!$('#nav-anchors a').hasClass('active')) {
+							$('#nav,#search').hide(); //Hide full navigation and search
+				}          
+			}
+	}
 	
-  if (!mobile) { //If Not Mobile
-    $('[role="tabpanel"],#nav,#search').show(); //Show full navigation and search
-    loadAux(); //Load auxiliary content
-		 } else { //Hide 
-	              if(!$('#nav-anchors a').hasClass('active')) {
-	                    $('#nav,#search').hide(); //Hide full navigation and search
-			}          
-		}
+	//Toggle navigation for small screens
+	function setNav() {
+		var $anchorLinks = $('#nav-anchors').find('a');
+		$anchorLinks.click(function(e){
+			e.preventDefault();
+			var $this = $(this),
+				thisHref = $this.attr('href');
+			$('.reveal').hide();
+			if($this.hasClass('active')) {
+				$this.removeClass('active');
+				$(thisHref).hide();
+			} else {
+				$anchorLinks.removeClass('active');
+				$this.addClass('active');
+				$(thisHref).show();
+			}
+		});
 	}
 
-//Set up Image Carousel
+	//Set up Image Carousel
 	function setImg() {
 		var container = $('#img-container'),
 			nav = $('#product-img').find('nav'),
@@ -121,24 +152,24 @@ function checkMobile() {
 		buildGallery();
 	}
 
-//Set up Auxiliary content
-function loadAux() {
-	var $aux = $('.aux');
-    $aux.each(function(index) {
-		var $this = $(this);
-		var auxLink = $this.find('a');
-		var auxFragment = auxLink.attr('href');
-		var auxContent = $this.find('[role=tabpanel]');
-		if (auxContent.size()===0 && $this.hasClass('loaded')===false) {
-			loadContent(auxFragment,$this);
-		}
-    });
-}
+	//Set up Auxiliary content
+	function loadAux() {
+		var $aux = $('.aux');
+		$aux.each(function(index) {
+			var $this = $(this);
+			var auxLink = $this.find('a');
+			var auxFragment = auxLink.attr('href');
+			var auxContent = $this.find('[role=tabpanel]');
+			if (auxContent.size()===0 && $this.hasClass('loaded')===false) {
+				loadContent(auxFragment,$this);
+			}
+		});
+	}
 
-function loadContent(src,container) { // Load Tab Content
-    container.addClass('loaded');
-    $('<div role="tabpanel" />').load(src +' #content > div',function() {
-        $(this).appendTo(container);
-	});
-}
-}
+	function loadContent(src,container) { // Load Tab Content
+		container.addClass('loaded');
+		$('<div role="tabpanel" />').load(src +' #content > div',function() {
+			$(this).appendTo(container);
+		});
+	}
+})(this);
